ed3dea806dfbcfbab56c2855ba9e687163854db2
Pranavwadghule43 <pranavwadghule43@gmail.com>
2026-03-01 19:33:12 +0530

    Fixed background path

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,6 @@
 import basket_icon from "./basket_icon.png";
 import logo from "./logo.png";
-import header_img from "./header_img.png";
+// import header_img from "./header_img.pn";
 import search_icon from "./search_icon.png";
 import menu_1 from "./menu_1.png";
 import menu_2 from "./menu_2.png";
@@ -84,7 +84,7 @@ import awards_2 from "./awards_2.png";
 export const assets = {
   logo,
   basket_icon,
-  header_img,
+  // header_img,
   search_icon,
   rating_starts,
   add_icon_green,
