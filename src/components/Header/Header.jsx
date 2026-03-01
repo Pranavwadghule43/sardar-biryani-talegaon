14c8cd73c6fa33e7f20fa38774aa6cc855ef41d6
Pranavwadghule43 <pranavwadghule43@gmail.com>
2026-03-01 20:12:49 +0530

    Updated Header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Header.css";
+import header_img from "../../assets/header_img.png"
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="header" style={{backgroundImage: URL(${header_img})}}>
       <div className="header-contents">
         <h2>Order your favourite food here.</h2>
         <p>
